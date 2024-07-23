418996e12de7c1759accefb9307289ce6fd421fc
Jangwon0319 <ryujw0319@gmail.com>
2024-07-23 17:10:47 +0900

    ♻️ Refactor: Reservation 엔티티 수정

--- src/main/java/roomescape/domain/Reservation.java
+++ src/main/java/roomescape/domain/Reservation.java
@@ -9,4 +9,36 @@ public class Reservation {
     private String time;
 
 
+    public Reservation(Long id, String name, String date, String time) {
+        this.id = id;
+        this.name = name;
+        this.date = date;
+        this.time = time;
+    }
+
+    public Reservation(String name, String date, String time) {
+        this.name = name;
+        this.date = date;
+        this.time = time;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public static Reservation toEntity(Reservation reservation, Long id){
+        return new Reservation(id, reservation.getName(), reservation.getDate(), reservation.getTime());
+    }
 }
