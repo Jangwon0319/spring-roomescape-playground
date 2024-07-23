7d098ba35d3722732c9e19f336240c03ae80d06b
Jangwon0319 <ryujw0319@gmail.com>
2024-07-23 17:16:17 +0900

    ✨ Feat: 예약 추가 API 구현

--- src/main/java/roomescape/controller/ReservationController.java
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -3,16 +3,20 @@ package roomescape.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import roomescape.domain.Reservation;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
 
 @Controller
 public class ReservationController {
 
+    private AtomicLong index = new AtomicLong(1);
     private List<Reservation> reservations = new ArrayList<>();
 
     // 예약 관리 페이지 응답
@@ -26,5 +30,15 @@ public class ReservationController {
     public ResponseEntity<List<Reservation>> reservations() {
         return ResponseEntity.ok().body(reservations);
     }
+    
+    // 예약 추가 API 구현
+    @PostMapping("/reservations")
+    public ResponseEntity<Void> createReservation(@RequestBody Reservation reservation) {
+        Reservation newReservation = Reservation.toEntity(reservation, index.getAndIncrement());
+        reservations.add(newReservation);
+        return ResponseEntity.created(URI.create("/reservations/" + newReservation.getId())).build();
+    }
+
+
 
 }
