a909082e1af9420a9699cb416f295c7f98c0d85e
Jangwon0319 <ryujw0319@gmail.com>
2024-08-11 14:14:22 +0900

    ✨ 7단계 - 데이터 추가/삭제 구현

--- src/main/java/roomescape/controller/ReservationController.java
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -3,12 +3,15 @@ package roomescape.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import roomescape.domain.Reservation;
 import roomescape.exception.NotFoundException;
 
 import java.net.URI;
+import java.sql.PreparedStatement;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -61,20 +64,42 @@ public class ReservationController {
             throw new IllegalArgumentException("Reservation Time cannot be null");
         }
 
-        Reservation newReservation = Reservation.toEntity(reservation, index.getAndIncrement());
-        reservations.add(newReservation);
+//        Reservation newReservation = Reservation.toEntity(reservation, index.getAndIncrement());
+//        reservations.add(newReservation);
+
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+
+        String sql = "INSERT INTO reservation (name, date, time) VALUES (?, ?, ?)";
+        jdbcTemplate.update(connection -> {
+            PreparedStatement ps = connection.prepareStatement(
+                    sql, new String[]{"id"});
+            ps.setString(1, reservation.getName());
+            ps.setString(2, reservation.getDate());
+            ps.setString(3, reservation.getTime());
+            return ps;
+        }, keyHolder);
+
+        Long newId = keyHolder.getKey().longValue();
+
+        Reservation newReservation = new Reservation(newId, reservation.getName(), reservation.getDate(), reservation.getTime());
+
+
         return ResponseEntity.created(URI.create("/reservations/" + newReservation.getId())).body(newReservation);
     }
 
     // 예약 취소 API 구현
     @DeleteMapping("/reservations/{id}")
     public ResponseEntity<Void> deleteReservation(@PathVariable long id) {
-        Reservation reservation = reservations.stream()
-                .filter(it -> Objects.equals(it.getId(), id))
-                .findFirst()
-                .orElseThrow(() -> new NotFoundException("Reservation not found"));
+//        Reservation reservation = reservations.stream()
+//                .filter(it -> Objects.equals(it.getId(), id))
+//                .findFirst()
+//                .orElseThrow(() -> new NotFoundException("Reservation not found"));
+//
+//        reservations.remove(reservation);
 
-        reservations.remove(reservation);
+        String sql = "DELETE FROM reservation WHERE id = ?";
+        int rowsAffected = jdbcTemplate.update(sql, id);
+
 
         return ResponseEntity.noContent().build();
     }
